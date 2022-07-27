ee6a6e7b17161af78ffd40dd2b0ff68ef87d50f7
AlfonsoValle <valleuned@gmail.com>
2022-07-27 09:08:26 +0200

    wip api

--- Backend/src/shopifyApi.ts
+++ Backend/src/shopifyApi.ts
@@ -5,7 +5,7 @@ export const shopifyApi: FastifyPluginAsync = async (app) => {
 	app.get("/sibarita", async (req, res) => {
 		const storefrontAccessToken = "a49676320ff0612be117f3891cd0d138 ";
 		const shop = "cafes-el-sibarita.myshopify.com";
-      
+
 		const storefrontClient = new Shopify.Clients.Storefront(shop, storefrontAccessToken);
 		const products = await storefrontClient.query({
 			data: ` {
