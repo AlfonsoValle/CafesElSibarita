6859f09d156c2b492c237d576480c62b3416e811
AlfonsoValle <valleuned@gmail.com>
2022-08-22 06:49:45 +0200

    crud is over

---
+++ Backend/src/routes/Specialities/deleteSpecialities.ts
@@ -0,0 +1,19 @@
+import { FastifyPluginAsync } from "fastify";
+import { specialityModel } from "./speciality.model";
+
+export const deleteSpecialities: FastifyPluginAsync = async (app) => {
+	app.delete<{ Params: { _id: string } }>("/deleteSpecialities/:_id", async (req, res) => {
+      const { _id } = req.params;
+		await specialityModel.findByIdAndDelete({_id: _id});
+
+      return {
+         status: 'deleted',
+     };
+	});
+};
+
+
+
+
+
+
--- Backend/src/routes/Specialities/specialitiesApp.ts
+++ Backend/src/routes/Specialities/specialitiesApp.ts
@@ -1,8 +1,10 @@
 import { FastifyPluginAsync } from "fastify";
 import { specialities } from "./specialities";
 import { createSpecialities } from "./createSpecialities";
+import { deleteSpecialities } from "./deleteSpecialities";
 
 export const specialitiesApp: FastifyPluginAsync = async (app) => {
 	app.register(specialities);
 	app.register(createSpecialities);
+	app.register(deleteSpecialities);
 };
